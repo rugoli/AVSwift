b7d539b2c0e308add7b0686ae7b9963772e31f6c
Roshan <none@none>
2018-01-01 19:03:09 -0500

    adding new framework for avswift

---
+++ AVSwift/AVSwift/AVQueryBuilder.swift
@@ -0,0 +1,13 @@
+//
+//  AVQueryBuilder.swift
+//  AVSwift
+//
+//  Created by Roshan on 1/2/18.
+//  Copyright © 2018 Roshan. All rights reserved.
+//
+
+import UIKit
+
+public class AVQueryBuilder: NSObject {
+
+}
