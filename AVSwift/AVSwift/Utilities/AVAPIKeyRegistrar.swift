8a6e08870ad29398958d64cf42f33cc109bd2936
Roshan <none@none>
2018-01-13 20:42:30 -0800

    Adding an API key registrar

---
+++ AVSwift/AVSwift/Utilities/AVAPIKeyRegistrar.swift
@@ -0,0 +1,50 @@
+//
+//  AVAPIKeyRegistrar.swift
+//  AVSwift
+//
+//  Created by Roshan Goli on 1/13/18.
+//  Copyright © 2018 Roshan. All rights reserved.
+//
+
+import UIKit
+
+public enum AVAPIRegistrarError: Error {
+  case invalidKey
+}
+
+public class AVAPIKeyRegistrar: NSObject {
+  private var apiKey: String?
+  
+  public func getAPIKey() -> String? {
+    return apiKey
+  }
+  
+  private func registerAPIKey(apiKey: String?) throws {
+    guard let key = apiKey else {
+      throw AVAPIRegistrarError.invalidKey
+    }
+    
+    // register key
+  }
+  
+  public func requestUserInputAPIKey(forViewController vc: UIViewController) {
+    let alertView = UIAlertController(title: "Enter API key", message: nil, preferredStyle: .alert)
+    
+    alertView.addTextField { textField in
+      textField.text = "API key"
+    }
+    
+    alertView.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alertView, weak self] (_) in
+      let textField = alertView?.textFields![0]
+      try! self?.registerAPIKey(apiKey: textField?.text)
+      alertView?.dismiss(animated: true, completion: nil)
+    }))
+    
+    alertView.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { [weak alertView] (_) in
+      alertView?.dismiss(animated: true, completion: nil)
+    }))
+    
+   vc.present(alertView, animated: true, completion: nil)
+  }
+
+}
--- AVTestApp/AVTestApp/ViewController.swift
+++ AVTestApp/AVTestApp/ViewController.swift
@@ -12,20 +12,12 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    var testBuilder = AVHistoricalStockPricesQueryBuilder()
+    var testBuilder = AVHistoricalStockPricesBuilder()
     testBuilder
       .setSymbol("MSFT")
       .setOuputSize(.full)
-      .setDataOutputType(.raw)
     print(testBuilder)
     // Do any additional setup after loading the view, typically from a nib.
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
 }
 
