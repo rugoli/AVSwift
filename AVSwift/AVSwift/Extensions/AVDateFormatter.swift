136fa3b1bc96326d87f5d9899d5cf7e25944814a
Roshan <none@none>
2018-02-24 21:30:22 -0800

    Starting to support date filtering

--- AVSwift/AVSwift/Extensions/AVDateFormatter.swift
+++ AVSwift/AVSwift/Extensions/AVDateFormatter.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-enum AVDateFormatterError: Error {
+public enum AVDateFormatterError: Error {
   case invalidString(String)
 }
 
@@ -20,5 +20,21 @@ extension String {
       throw AVDateFormatterError.invalidString("Invalid date string: \(self)")
     }
     return date
+  }
+}
+
+extension Date {
+  static func from(month: Int, day: Int, year: Int, withFallback fallback: Date?) throws -> Date {
+    let monthFormat = String(format: "%02d", month)
+    let dayFormat = String(format: "%02d", day)
+    do {
+      let date = try "\(year)-\(monthFormat)-\(dayFormat)".toDate()
+      return date
+    } catch {
+      guard let fallback = fallback else {
+        throw error
+      }
+      return fallback
+    }
   }
 }
